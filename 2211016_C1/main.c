175595e3c87f66e83c8272f37d7f8b4064b72bb1
seannabe <115057870+seannabe@users.noreply.github.com>
2022-11-16 11:59:39 +0800

    C_221116

---
+++ 2211016_C1/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	
+	
+	
+	
+	
+	
+	
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+////////////////////////////////////////////////////////////////////////////////////// 
+/*	
+***** *****
+****   ****
+***     ***
+**       **
+*         *
+**       **
+***     ***
+****   ****
+***** *****
+*/
+/* 
+	int width = 0;
+	int left;
+	int right;
+	int i;
+	int k;
+	
+	for (i=0; i<9; i++){           //ª½ 
+		for (k=0; k<11; k++){	   //¾î 
+			left = 5 - width;
+			right = 5 + width;
+			if (k>=left && k<=right)
+				printf(" ");
+			else
+				printf("*");
+		}
+		if (i < 4)
+			width++;
+		else
+			width--;
+		printf("\n");
+	}
+	
+	return 0;
+}
+*/ 
